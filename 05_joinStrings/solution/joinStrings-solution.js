f704e5e9a305fc445de93b9782d45a44c7ed23cb
Leslie Mujica <35012536+lamujica@users.noreply.github.com>
2025-07-28 19:49:31 -0400

    Fix js file to pass spec.js tests

--- 05_joinStrings/solution/joinStrings-solution.js
+++ 05_joinStrings/solution/joinStrings-solution.js
@@ -20,16 +20,12 @@ const age = thisYear - birthYear;
 // At the end of the exercise, greeting may look like this:
 const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
-
-
-
-
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  firstName: typeof firstName === "undefined" ? undefined : firstName,
+  lastName: typeof lastName === "undefined" ? undefined : lastName,
+  thisYear: typeof thisYear === "undefined" ? undefined : thisYear,
+  birthYear: typeof birthYear === "undefined" ? undefined : birthYear,
+  greeting: typeof greeting === "undefined" ? undefined : greeting,
+  fullName: typeof fullName === "undefined" ? undefined : fullName,
+  age: typeof age === "undefined" ? undefined : age,
+};
